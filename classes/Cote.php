310e4e00cbd16a2ef2ce65600d0dd4cf2efcaa66
kokmok <jona303@gmail.com>
2017-02-02 15:36:25 +0100

    ajust Cote getter and setters

--- classes/Cote.php
+++ classes/Cote.php
@@ -2,7 +2,53 @@
 
 class Cote
 {
-    public $nomDuCours;
-    public $valueInPercent;
+    private $nomDuCours;
+    private $valueInPercent;
+
+    /**
+     * Cote constructor.
+     * @param $nomDuCours
+     * @param $valueInPercent
+     */
+    public function __construct($nomDuCours, $valueInPercent)
+    {
+        $this->setNomDuCours($nomDuCours);
+        $this->setValueInPercent($valueInPercent);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomDuCours()
+    {
+        return $this->nomDuCours;
+    }
+
+    /**
+     * @param mixed $nomDuCours
+     */
+    public function setNomDuCours($nomDuCours)
+    {
+        $this->nomDuCours = $nomDuCours;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValueInPercent()
+    {
+        return $this->valueInPercent;
+    }
+
+    /**
+     * @param mixed $valueInPercent
+     */
+    public function setValueInPercent($valueInPercent)
+    {
+        $this->valueInPercent = $valueInPercent;
+    }
     
+    
+
+
 }
